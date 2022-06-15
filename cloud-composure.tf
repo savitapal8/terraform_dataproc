ef68d067d78658ca3a217de461f1842cd80a9e2e
Savita Pal <savita.pal08@gmail.com>
2022-06-15 21:54:40 +0530

    Update cloud-composure.tf

--- cloud-composure.tf
+++ cloud-composure.tf
@@ -8,7 +8,8 @@ module "composer" {
   network_project_id = var.network_project_id
   subnetwork = var.subnetwork
   subnetwork_region = var.subnetwork_region
-  composer_service_account = var.composer_service_account
+  #composer_service_account = var.composer_service_account
+  composer_service_account = null
   pod_ip_allocation_range_name = var.pod_ip_allocation_range_name
   service_ip_allocation_range_name = var.service_ip_allocation_range_name
   airflow_config_overrides = var.airflow_config_overrides
