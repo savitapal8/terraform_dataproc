739cb2c859e0c613d11ad642581b4d12287aa803
Savita Pal <savita.pal08@gmail.com>
2022-06-19 21:08:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ provider "google-beta"{
   access_token          = var.access_token
 }
 
-#resource "google_service_account" "cloud-composer-sa" {
-#  account_id   = "cloud-composer-sa-id"
-#  display_name = "Service Account"
-#  project = var.project
-#}
+resource "google_service_account" "cloud-composer-sa" {
+  account_id   = "cloud-composer-sa-id"
+  display_name = "Service Account"
+  project = var.project
+}
 
-#resource "google_project_iam_member" "cloud-composer" {
-#  project = var.project
-#  role    = "roles/composer.ServiceAgentV2Ext"
-#  member  = "serviceAccount:${google_service_account.cloud-composer-sa.email}"
-#}
+resource "google_project_iam_member" "cloud-composer" {
+  project = var.project
+  role    = "roles/composer.ServiceAgentV2Ext"
+  member  = "serviceAccount:${google_service_account.cloud-composer-sa.email}"
+}
