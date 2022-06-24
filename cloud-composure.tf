4ab4538fe8150f75c0f6fb72c0eebd1c916fc769
Savita Pal <savita.pal08@gmail.com>
2022-06-24 14:06:41 +0530

    Update cloud-composure.tf

--- cloud-composure.tf
+++ cloud-composure.tf
@@ -131,34 +131,20 @@ resource "google_composer_environment" "composer_env" {
       }
     }
 
-    #dynamic "master_authorized_networks_config" {
-    #  for_each = local.master_authorized_networks_config
-    #  content {
-    #    enabled = length(var.master_authorized_networks) > 0
-    #    dynamic "cidr_blocks" {
-    #      for_each = master_authorized_networks_config.value["cidr_blocks"]
-    #      content {
-    #        cidr_block   = master_authorized_networks_config.value["cidr_block"]
-    #        display_name = master_authorized_networks_config.value["display_name"]
-    #      }
-    #    }
-    #  }
-    #}    
-    
-    master_authorized_networks_config {
-        enabled = true
-	cidr_blocks = [
-	    {
-		cidr_block = "8.8.8.8/32"
-		display_name = "Net 1"
-	    },
-	    {
-		cidr_block = "4.4.4.4/32"
-		display_name = "something"
-	    },
-	]
-        
+    dynamic "master_authorized_networks_config" {
+      for_each = local.master_authorized_networks_config
+      content {
+        enabled = length(var.master_authorized_networks) > 0
+        dynamic "cidr_blocks" {
+          for_each = master_authorized_networks_config.value["cidr_blocks"]
+          content {
+            cidr_block   = master_authorized_networks_config.value["cidr_block"]
+            display_name = master_authorized_networks_config.value["display_name"]
+          }
+        }
+      }
     }
+   
     
   }
 
